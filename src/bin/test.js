355bebc3fc98f03846bbdad48e5d212f05f26472
Lebedev Artem <exces-st@yandex.ru>
2017-11-21 23:23:17 +0300

    finish step 5

--- src/bin/test.js
+++
@@ -1,7 +0,0 @@
-const randomInt = (min, max) => {
-  const random = ((min - 0.5) + Math.random()) * ((max - min));
-  console.log(Math.round(random));
-  return Math.round(random);
-};
-
-randomInt(1, 3);
